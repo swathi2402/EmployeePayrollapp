445b90c8ee01430e3bbaef9ad4c8f2d170c3c457
Swathi <swathi.h@perfios.com>
2021-10-13 19:19:42 +0530

    MODIFIED: Displaying all payroll details from JSON object

--- js/new_home.js
+++ js/new_home.js
@@ -4,21 +4,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>"
-    let employeePayrollData = createEmployeePayrollJSON()[0];
-    const innerHtml = `${headerHtml}
-        <tr>
-            <td><img class="profile" alt="" src="${employeePayrollData._profilePic}"></td>
-            <td>${employeePayrollData._name}</td>
-            <td>${employeePayrollData._gender}</td>
-            <td>${getDeptHtml(employeePayrollData._department)}</td>
-            <td>${employeePayrollData._salary}</td>
-            <td>${employeePayrollData._startDate}</td>
-            <td>
-                <img name="${employeePayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-                <img name="${employeePayrollData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
-            </td>
-        </tr>
-    `;
+    let innerHtml = `${headerHtml}`;
+    let employeePayrollList = createEmployeePayrollJSON();
+    for (const employeePayrollData of employeePayrollList) {
+        innerHtml = `${headerHtml}
+            <tr>
+                <td><img class="profile" alt="" src="${employeePayrollData._profilePic}"></td>
+                <td>${employeePayrollData._name}</td>
+                <td>${employeePayrollData._gender}</td>
+                <td>${getDeptHtml(employeePayrollData._department)}</td>
+                <td>${employeePayrollData._salary}</td>
+                <td>${employeePayrollData._startDate}</td>
+                <td>
+                    <img name="${employeePayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                    <img name="${employeePayrollData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+                </td>
+            </tr>
+        `;
+    }
     document.querySelector('#display').innerHTML = innerHtml;
 }
 
