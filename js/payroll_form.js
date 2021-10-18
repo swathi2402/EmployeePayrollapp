51b2f8ed519e45f5bf92afc9636433869058f119
Swathi <swathi.h@perfios.com>
2021-10-18 19:49:16 +0530

    MODIFIED: Adding and updating employee payroll service through JSON server

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -67,12 +67,33 @@ const save = (event) => {
     event.stopPropagation();
     try {
         setEmployeePayrollObject();
-        createAndUpdateStorage();
-        resetForm();
-        window.location.replace(site_properties.home_page);
+        if(site_properties.use_local_storage.match("true")) { 
+            createAndUpdateStorage();
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        } else {
+            createOrUpdateEmployeePayroll();
+        }
     } catch (error) {
         return;
     }
+}
+
+const createOrUpdateEmployeePayroll = () => {
+    let postUrl = site_properties.server_url;
+    let methodCall = "POST";
+    if (isUpdate) {
+        methodCall = "PUT";
+        postUrl = postUrl + employeePayrollObj.id.toString();
+    }
+    makePromisecall(methodCall, postUrl, true, employeePayrollObj)
+        .then(responseText => {
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        })
+        .catch (error => {
+            throw error();
+        });
 }
 
 const setEmployeePayrollObject = () => {
