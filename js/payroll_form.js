11d519517f25a6fa2fbf3a4ad870a47abf9c0e51
Swathi <swathi.h@perfios.com>
2021-10-18 20:07:39 +0530

    REFACTORED: Moved name and date validate method to utility file

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -46,22 +46,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     checkForUpdate();
   });
 
-const checkName = (name) => {
-    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
-        if(!nameRegex.test(name)) 
-            throw 'Name is incorrect';
-}
-
-const checkStartDate = (startDate) => {
-    let now = new Date();
-        if (startDate > now) 
-            throw 'Start date is future date';
-        let oneMonthSpan = new Date();
-        oneMonthSpan.setDate(oneMonthSpan.getDate() - 30);
-        if (startDate < oneMonthSpan)
-            throw 'Start date is beyond 30 days';
-}
-
 const save = (event) => {
     event.preventDefault();
     event.stopPropagation();
--- js/utility.js
+++ js/utility.js
@@ -2,4 +2,20 @@ const stringifyDate = (date) => {
     const options = { day: 'numeric', month: 'short', year: 'numeric' };
     const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
     return newDate;
-  }
+}
+
+const checkName = (name) => {
+  let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+      if(!nameRegex.test(name)) 
+          throw 'Name is incorrect';
+}
+
+const checkStartDate = (startDate) => {
+  let now = new Date();
+      if (startDate > now) 
+          throw 'Start date is future date';
+      let oneMonthSpan = new Date();
+      oneMonthSpan.setDate(oneMonthSpan.getDate() - 30);
+      if (startDate < oneMonthSpan)
+          throw 'Start date is beyond 30 days';
+}
