b349bf5bc20cee1b90ecfb25da688afe6aad01fa
Swathi <swathi.h@perfios.com>
2021-10-18 19:09:59 +0530

    MODIFIED: Adding url to site properties

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
 let site_properties = {
+    use_local_storage: "true",
     home_page: "../pages/home.html",
-    add_employee_payroll_page: "../pages/employee_payroll_form.html"
+    add_employee_payroll_page: "../pages/employee_payroll_form.html",
+    server_url:"http://localhost:3000/EmployeePayrollDB/"
   };
